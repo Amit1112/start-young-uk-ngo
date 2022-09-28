ee06c4373bb6c1cd861c239c4a47af417baa3d56
Amit Vishwakarma <amitv094@gmail.com>
2022-09-29 00:31:01 +0530

    Fix for routing issue in azure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
+  { path: '', component: DashboardComponent, pathMatch: 'full' },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
-  { path: 'signUp', component: RegistrationComponent }
+  { path: 'signUp', component: RegistrationComponent },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
