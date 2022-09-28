df10716a1d72579e1e9ed440010ce3138c09dc44
ULTIMATE <mayurthorat1997@gmail.com>
2022-09-29 05:02:49 +0530

    Regestration form changes

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -30,7 +30,7 @@ export class RegistrationComponent implements OnInit {
     dob: new FormControl('', Validators.required),
     emailId: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
     password: new FormControl('', Validators.required),
-    subscribe: new FormControl('')
+    subscribe: new FormControl(true)
   });
 
   constructor() { }
