3ce95a7fb651d6b85706e35e209e0479dbdf86de
ULTIMATE <mayurthorat1997@gmail.com>
2022-09-29 03:50:18 +0530

    Admin forms validations

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -29,7 +29,9 @@ export class RegistrationComponent implements OnInit {
     city: new FormControl('', Validators.required),
     state: new FormControl('', Validators.required),
     dob: new FormControl('', Validators.required),
-    emailId: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")])
+    emailId: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    password: new FormControl('', Validators.required),
+    subscribe: new FormControl('')
   });
 
   constructor() { }
